d999e537beae21b4b7875182271cdda2e6fd2313
RajabM1 <rajab1.masri@gmail.com>
2023-03-28 20:20:28 +0300

    Prepare the project skeleton

---
+++ MdawemApp/Views/TabbedPage1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MdawemApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage
+    {
+        public TabbedPage1 ()
+        {
+            InitializeComponent();
+            Children.Add(new NavigationPage(new HomePage()) { Title = "Home", IconImageSource = "home.png" });
+            Children.Add(new NavigationPage(new ProfilePage()) { Title = "Profile", IconImageSource = "profile.png" });
+            Children.Add(new NavigationPage(new notificationPage()) { Title = "Notification", IconImageSource = "notification.png" });
+            Children.Add(new NavigationPage(new FlyoutPage1Flyout()) { Title = "MENU", IconImageSource = "menu.png" });
+        }
+    }
+}
