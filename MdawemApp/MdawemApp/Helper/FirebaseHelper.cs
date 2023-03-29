1cad61acdcbbdfd90c4bc11445aaef97df66f49e
git config --global user.namNoorFares <s12028537@stu.najah.edu>
2023-03-29 20:11:54 +0300

    Add a logic for login page

--- MdawemApp/MdawemApp/Helper/FirebaseHelper.cs
+++ MdawemApp/MdawemApp/Helper/FirebaseHelper.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace MdawemApp.Helper
 {
@@ -10,9 +12,29 @@ namespace MdawemApp.Helper
         string webAPIKey = "AIzaSyDuxpf83oL4rNwmPBV06DEid9xUWPNyOWU";
         FirebaseAuthProvider authProvider;
 
-        public  FirebaseHelper()
+        public FirebaseHelper()
         {
             authProvider = new FirebaseAuthProvider(new FirebaseConfig(webAPIKey));
         }
+
+        public async Task<string> Login(string email, string password)
+        {
+            var token = await authProvider.SignInWithEmailAndPasswordAsync(email, password);
+            if (!string.IsNullOrEmpty(token.FirebaseToken))
+            {
+                return token.FirebaseToken;
+            }
+            {
+                return "";
+            }
+        }
+
+        public void SignOut()
+        {
+            Application.Current.Properties.Remove("emailtxt");
+            Application.Current.Properties.Remove("passwordtxt");
+        }
     }
 }
+
+
