6a3faf22810134ff389b81afe7e2494ddfc8221b
NoorFares <92647638+NoorFares@users.noreply.github.com>
2023-04-13 01:47:53 +0300

    Update VactionRequestModel.cs

--- MdawemApp/MdawemApp/Models/VactionRequestModel.cs
+++ MdawemApp/MdawemApp/Models/VactionRequestModel.cs
@@ -7,8 +7,8 @@ namespace MdawemApp.Models
     public class VactionReuestModel
     {
         public string Type { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
         public string Reason { get; set; }
 
         public string _status { get; set; }
