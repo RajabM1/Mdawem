0999ce242dc5cd9e29d7b017ba60cb8ae8b1a13e
RajabM1 <rajab1.masri@gmail.com>
2023-04-19 15:16:02 +0300

    Add a Design & Logic For Supervisor Notification Page

---
+++ MdawemApp/MdawemApp/Models/NotificationData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MdawemApp.Models
+{
+    public class NotificationData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfRequest { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public string Reason { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string UserId { get; set; }
+        public string LeaveId { get; set; }
+
+    }
+}
