e0ab1b2eb1732c4c40858e4ad62c4482094423a7
git config --global user.namNoorFares <s12028537@stu.najah.edu>
2023-04-19 14:10:02 +0300

    Design and logic the ProfilePage

---
+++ MdawemApp/MdawemApp/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MdawemApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MdawemApp.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public List<Employee> Employees { get; set; }
+    }
+
+}
